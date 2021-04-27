836511287304ca03ba03f43f801392e54bc0fa28
Manuel <manuel.penya.trabajo@gmail.com>
2021-04-27 08:51:26 +0200

    Header style OK. Project FINISHED

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './header.scss';
+import myPhoto from './../../img/unnamed.png'
 
 const Header = () => {
     return(
         <header>
-            <div className="header-left-part"><span>Left</span></div>
-            <div className="header-center-part"><img src="./../../img/09-12.jpg" alt="My-Trello" /></div>
-            <div className="header-right-part"><span>Right</span></div>
+            <div className="header-left-part"><span>Manuel</span></div>
+            <div className="header-center-part">
+                <p>My Trello</p>
+            </div>
+            <div className="header-right-part">
+                <img src={myPhoto} alt="personal"/>
+            </div>
         </header>
     );
 }
--- src/Containers/main/Main.jsx
+++ src/Containers/main/Main.jsx
@@ -4,10 +4,6 @@ import AddList from '../../Components/AddList/AddList.jsx';
 import './Main.scss';
 
 const Main = () => {
-    //as Main has both lists and AddList components, 
-    //it needs to have the state, so that it can be 
-    //passed to both children and be used and modified in
-    //here so that it is reflected in both children
 
     const [lists,setLists] = useState([]);
 
